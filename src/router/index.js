579029d793a633e66ac169874d7e43caa564380b
rrAlfaro <rafael@alfaro.com.br>
2024-01-25 10:01:59 -0300

    final

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import TestesView from '@/views/TestesView.vue'
-import PortfolioTesteView from '@/views/PortfolioTesteView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,17 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/testes',
-      name: 'testes',
-      component: TestesView
-    },
-    {
-      path: '/portifolio',
-      name: 'portifolio',
-      component: PortfolioTesteView
-    },
+    }
   ]
 })
 
